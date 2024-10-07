ae1a3b33835ead1e7a8ce910f5b84c9221857584
Antoshkin Artem <Artem.Antoshkin@infowatch.com>
2024-10-08 01:14:36 +0300

    try;

--- dist/index.js
+++ dist/index.js
@@ -30590,11 +30590,31 @@ async function run() {
       stage: 'DEPLOY'
     }
 
-    const response = await axios.post('https://way-of-believers.vercel.app/checkData', reqBody);
+    const response = await axios.post(
+      'https://way-of-believers.vercel.app/checkData',
+      reqBody,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          // Add any other required headers here
+        }
+      }
+    );
     console.log(response)
 
     core.setOutput('result', JSON.stringify(response.data));
   } catch (error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      core.setFailed(`Server responded with: ${error.response.status}`);
+      core.setFailed(`Response data: ${JSON.stringify(error.response.data)}`);
+    } else if (error.request) {
+      // The request was made but no response was received
+      core.setFailed(`No response received: ${error.request}`);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      core.setFailed(`Error setting up request: ${error.message}`);
+    }
     core.setFailed(error.message);
   }
 }
